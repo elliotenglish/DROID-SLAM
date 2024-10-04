3ab263b5ef8c94b4ebc3002e63d8fbfe2643204e
Elliot English <eenglish@gmail.com>
2024-10-04 02:34:13 -0700

    add notes on corr_index_forward i/o

--- tests/test_droid_backends.py
+++ tests/test_droid_backends.py
@@ -3,13 +3,24 @@ import droid_backends
 import numpy as np
 
 def test_corr_index_forward():
-  volume=torch.tensor(np.array([[1, 2, 3], [4, 5, 6]]))
-  coords=torch.tensor(np.array([[0,0]]))
-  radius=5
+  h1=30
+  w1=40
+  h2=h1
+  w2=w1
+  b=1
 
-  result=droid_backends.corr_index_forward(volume,coords,radius)
+  radius=3
+  volume=torch.tensor(np.ndarray([b,h1,w1,h2,w2],dtype=np.float32))
+  coords=torch.tensor(np.ndarray([b,2,h1,w1],dtype=np.float32))
+  #corr=torch.tensor(np.ndarray([b,2*radius+1,2*radius+1,h1,w1]))
 
-  print(result)
+  # volume is expected to be BxH1xW1xH2xW2, where the values are the correlation values.
+  # coords is expected to be BxCxH1xW1, where C is the coordinates of the frame 1 points projected in frame 2.
+  # corr is expected to be Bx(2*R+1)x(2*R+1)xH1xW1
+  corr=droid_backends.corr_index_forward(volume,coords,radius)
+  corr=np.array(corr)
+
+  print(corr.shape,corr.dtype)
 
 if __name__=="__main__":
   test_corr_index_forward()
