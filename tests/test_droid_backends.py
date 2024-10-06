5b34ee28e4147c15618503f5fafa7c5473bdc19e
Elliot English <elliotenglish@gmail.com>
2024-10-06 15:35:56 +0300

    add first corr cpu version

--- tests/test_droid_backends.py
+++ tests/test_droid_backends.py
@@ -1,6 +1,45 @@
 import torch
-import droid_backends
+#import droid_backends
 import numpy as np
+import math
+
+def within_bounds(y,x,h,w):
+  return y>0 and y<h and x>0 and x<w
+
+def corr_index_forward(volume,coords,radius):
+  N,h1,w1,h2,w2=volume.shape
+  r=radius
+
+  rd=2*r+1
+
+  corr=np.ndarray([N,rd,rd,h1,w2],dtype=np.float32)
+
+  n=0
+  for x in range(w1):
+    for y in range(h1):
+
+      x0=coords[n,0,y,x]
+      y0=coords[n,1,y,x]
+
+      x0f=math.floor(x0)
+      y0f=math.floor(y0)
+
+      dx=x0-x0f
+      dy=y0-y0f
+
+      for i in range(rd+1):
+        for j in range(rd+1):
+          x1=x0f-r+i
+          y1=y0f-r+j
+
+          if within_bounds(y1,x1,h2,w2):
+            s=volume[n,y,x,y1,x1]
+            if i>0 and j>0: corr[n,i-1,j-1,y,x]+=s*(dx*dy)
+            if i>0 and j<rd: corr[n,i-1,j,y,x]+=s*(dx*(1-dy))
+            if i<rd and j>0: corr[n,i,j-1,y,x]+=s*((1-dx)*dy)
+            if i<rd and j<rd: corr[n,i,j,y,x]+=s*((1-dx)*(1-dy))
+
+  return corr
 
 def test_corr_index_forward():
   h1=3
@@ -13,7 +52,7 @@ def test_corr_index_forward():
 
   radius=0
   volume=torch.tensor(gen.uniform(-1,1,size=[b,h1,w1,h2,w2]).astype(np.float32))
-  coords=torch.tensor(np.concat(
+  coords=torch.tensor(np.concatenate(
     [
       gen.uniform(0,w2-1,size=[b,1,h1,w1]).astype(np.float32),
       gen.uniform(0,h2-1,size=[b,1,h1,w1]).astype(np.float32)
@@ -27,15 +66,18 @@ def test_corr_index_forward():
   print("radius",radius)
 
   cpu_device = torch.device("cpu")
-  gpu_device = torch.device("cuda")
-  volume=volume.to(device=gpu_device)
-  coords=coords.to(device=gpu_device)
+  #gpu_device = torch.device("cuda")
+  #volume=volume.to(device=gpu_device)
+  #coords=coords.to(device=gpu_device)
 
   # volume is expected to be BxH1xW1xH2xW2, where the values are the correlation values.
   # coords is expected to be BxCxH1xW1, where C is the coordinates of the frame 1 points projected in frame 2. The coordinates are x,y order rather than the y,x ordered used
   # corr is expected to be Bx(2*R+1)x(2*R+1)xH1xW1
-  corr=droid_backends.corr_index_forward(volume,coords,radius)[0]
-  corr=corr.to(device=cpu_device)
+  
+  #corr=droid_backends.corr_index_forward(volume,coords,radius)[0]
+  corr=corr_index_forward(volume,coords,radius)[0]
+  
+  #corr=corr.to(device=cpu_device)
   corr=np.array(corr)
 
   print(corr)
