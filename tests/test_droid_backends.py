7035511349362f66c70cdb2f3d450fe99fb52d1f
Elliot English <elliotenglish@gmail.com>
2024-10-06 20:05:33 +0300

    correct corr index kernel

--- tests/test_droid_backends.py
+++ tests/test_droid_backends.py
@@ -3,8 +3,8 @@ import torch
 import numpy as np
 import math
 
-def within_bounds(y,x,h,w):
-  return y>0 and y<h and x>0 and x<w
+def within_bounds(h,w,H,W):
+  return h >= 0 and h < H and w >= 0 and w < W
 
 def corr_index_forward(volume,coords,radius):
   N,h1,w1,h2,w2=volume.shape
@@ -12,7 +12,10 @@ def corr_index_forward(volume,coords,radius):
 
   rd=2*r+1
 
-  corr=np.ndarray([N,rd,rd,h1,w2],dtype=np.float32)
+  corr=np.ndarray([N,rd,rd,h1,w1],dtype=np.float32)
+  corr[...]=0
+
+  # cnt=0
 
   n=0
   for x in range(w1):
@@ -21,23 +24,38 @@ def corr_index_forward(volume,coords,radius):
       x0=coords[n,0,y,x]
       y0=coords[n,1,y,x]
 
-      x0f=math.floor(x0)
-      y0f=math.floor(y0)
+      x0f=int(math.floor(x0))
+      y0f=int(math.floor(y0))
 
       dx=x0-x0f
       dy=y0-y0f
 
+      # print(dx,dy)
+
       for i in range(rd+1):
         for j in range(rd+1):
-          x1=x0f-r+i
-          y1=y0f-r+j
+          x1=x0f - r + i
+          y1=y0f - r + j
+
+          # cnt+=1
 
           if within_bounds(y1,x1,h2,w2):
             s=volume[n,y,x,y1,x1]
-            if i>0 and j>0: corr[n,i-1,j-1,y,x]+=s*(dx*dy)
-            if i>0 and j<rd: corr[n,i-1,j,y,x]+=s*(dx*(1-dy))
-            if i<rd and j>0: corr[n,i,j-1,y,x]+=s*((1-dx)*dy)
-            if i<rd and j<rd: corr[n,i,j,y,x]+=s*((1-dx)*(1-dy))
+            #print(s)
+
+            if i > 0 and j > 0:
+              corr[n,i-1,j-1,y,x] += s * (dx * dy)
+
+            if i > 0 and j < rd:
+              corr[n,i-1,j,y,x] += s * (dx * (1-dy))
+
+            if i < rd and j > 0:
+              corr[n,i,j-1,y,x] += s * ((1-dx) * dy)
+
+            if i < rd and j < rd:
+              corr[n,i,j,y,x] += s * ((1-dx) * (1-dy))
+
+  # print("cnt",cnt)
 
   return corr
 
@@ -73,15 +91,14 @@ def test_corr_index_forward():
   # volume is expected to be BxH1xW1xH2xW2, where the values are the correlation values.
   # coords is expected to be BxCxH1xW1, where C is the coordinates of the frame 1 points projected in frame 2. The coordinates are x,y order rather than the y,x ordered used
   # corr is expected to be Bx(2*R+1)x(2*R+1)xH1xW1
-  
+
   #corr=droid_backends.corr_index_forward(volume,coords,radius)[0]
   corr=corr_index_forward(volume,coords,radius)[0]
-  
+
   #corr=corr.to(device=cpu_device)
   corr=np.array(corr)
 
-  print(corr)
-  print(corr.shape,corr.dtype)
+  print("corr",corr.shape,corr.dtype,corr)
 
 if __name__=="__main__":
   test_corr_index_forward()
